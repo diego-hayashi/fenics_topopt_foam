972961167c1881730a0b9cc667e39445b04f10fb
diego-hayashi <diego.alonso@usp.br>
2021-09-06 21:08:47 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@
 ############################### Python libraries ###############################
 
 import itertools
-import setuptools, find_packages
+import setuptools
 
 ############################# Project information ##############################
 
@@ -140,7 +140,7 @@ setuptools.setup(
 	# Packages
 	packages = [library_name],
 	package_dir = {
-		library_name : find_packages(library_name),
+		library_name : setuptools.find_packages(library_name),
 	},
 
 	############################# Requirements #############################
